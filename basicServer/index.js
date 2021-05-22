f7d9599b6aa2502bf04f4aeedf15746d54273ee3
shwetanshutech <shwetanshu2000@gmail.com>
2021-05-22 18:33:48 +0530

    http post request with fetch()

--- basicServer/index.js
+++ basicServer/index.js
@@ -8,4 +8,10 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 app.post("/api", (request, response) => {
   console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: "success",
+    latitude: data.lat,
+    longitude: data.long,
+  });
 });
