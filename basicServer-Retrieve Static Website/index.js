5e8d63a0de8a79d04828006089a42d73dbfaecb5
shwetanshutech <shwetanshu2000@gmail.com>
2021-05-22 17:48:29 +0530

    file rename

---
+++ basicServer-Retrieve Static Website/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
+  console.log("listenning at port 3000");
+});
+app.use(express.static("public"));
+app.post("/api", (request, response) => {
+  console.log(request);
+});
