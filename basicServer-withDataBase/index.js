b1b115cbd253f442752645d68e1616cd71f6573d
shwetanshutech <shwetanshu2000@gmail.com>
2021-05-22 21:23:53 +0530

    saving to DB added

--- basicServer-withDataBase/index.js
+++ basicServer-withDataBase/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
-
+const Datastore = require("nedb");
 app.listen(3000, () => {
   console.log("listenning at port 3000");
 });
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/api", (request, response) => {
-  console.log(request.body);
+  console.log("I got a request");
   const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
   response.json({
     status: "success",
+    timestamp: timestamp,
     latitude: data.lat,
     longitude: data.long,
   });
